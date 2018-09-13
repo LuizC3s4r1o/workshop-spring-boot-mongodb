275bb12f38a926a2de63798d94a4bb3ba2873223
Luiz Cesario <luiz.cesario@yahoo.com.br>
2018-09-12 23:30:53 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/lacsystem/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lacsystem/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lacsystem.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lacsystem.workshopmongo.domain.Post;
 import com.lacsystem.workshopmongo.domain.User;
+import com.lacsystem.workshopmongo.repository.PostRepository;
 import com.lacsystem.workshopmongo.repository.UserRepository;
 
 /**
@@ -19,11 +23,18 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyy");
+		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User luiz = new User(null, "Luiz Cesario", "luizC@gmail.com");
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -31,7 +42,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, simpleDateFormat.parse("10/09/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", luiz);
+		Post post2 = new Post(null, simpleDateFormat.parse("12/09/2018"), "Bom dia", "Acordei feliz hoje!", luiz);
+		
 		userRepository.saveAll(Arrays.asList(luiz, maria, gabi, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		
 	}
---
+++ src/main/java/com/lacsystem/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.lacsystem.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lacsystem.workshopmongo.domain.Post;
+
+/**
+ * @author Luiz.Cesario
+ *
+ */
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
